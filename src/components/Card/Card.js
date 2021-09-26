b79a74fe2cf80e131e2727367910fcab28963b55
unknown <anwarhosen575@gmail.com>
2021-09-26 07:15:01 +0600

    secound

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+const Card = () => {
+    return (
+        <div >
+            <div className="card">
+                <h2>card hfvbeoug jfbgwvhb uh</h2>
+            </div>
+            <h2>card hfvbeoug jfbgwvhb uh</h2>
+
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <h2>header </h2>
+            <p>TOP 100 BUSSINESS MAN IN THE WORLD</p>
+            <p>gyedvygy </p>
+
         </div>
     );
 };
